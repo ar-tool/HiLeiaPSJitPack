a8fba992d7cee0c19a5672189078a746426c94c7
yic <284277590@qq.com>
2024-02-19 10:08:17 +0800

    test
    update:增加测试

---
+++ library/src/main/java/com/hiar/hileiaps/TestUtil.java
@@ -0,0 +1,15 @@
+package com.hiar.hileiaps;
+
+/**
+ * Copyright (C)
+ * FileName: TestUtil
+ * Author: yic
+ * Date: 2024/2/19 10:04
+ * Description:
+ */
+public class TestUtil {
+
+    public static void test(){
+
+    }
+}
